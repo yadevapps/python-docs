77f6e915bd8efbdc4a5dd7da47593b4a90f6b1d6
yamigaming90 <yamigaming90@gmail.com>
2024-06-29 11:48:46 +0100

    Create py003-variable.py
    variable

---
+++ py003-variable.py
@@ -0,0 +1,21 @@
+# variable 
+var_int = 5
+var_str = "text"
+var_float = 5.2
+var_bool = True
+var_bool = False
+
+print(var_int,
+      var_str,
+      var_float,
+      var_bool)
+
+# multiple var in single line
+var_int, var_str, var_float, var_bool = 5, "text", 5.2, True
+x = y = z = 10
+
+print(var_int,
+      var_str,
+      var_float,
+      var_bool,
+      x, y, z)
