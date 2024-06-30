1389558867d1a5a4b2ba13127df484c5a86f19ea
yamigaming90 <yamigaming90@gmail.com>
2024-06-30 20:19:13 +0100

    Create py005-operation.py
    operation

---
+++ py005-operation.py
@@ -0,0 +1,18 @@
+x = 5
+y = 2
+
+Addition = x + y
+Subtraction = x - y
+Multiplication = x * y
+Division = x / y
+Modulus = x % y
+Exponentiation = x ** y
+Floor_division	= x // y
+
+print(  Addition,
+        Subtraction,
+        Multiplication,
+        Division,
+        Modulus,
+        Exponentiation,
+        Floor_division, )
